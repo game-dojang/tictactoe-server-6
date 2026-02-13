85eca1997838305b67890c83f4addaf6ca68dd3c
game-dojang <gamedojang@gmail.com>
2026-02-13 11:23:00 +0900

    로그인 구현

--- routes/users.js
+++ routes/users.js
@@ -81,7 +81,6 @@ router.post('/signin', async function(req, res, next) {
     } else {
       res.status(404).json({ message: 'User not found.' });
     }
-
   } catch (error) {
     res.status(500).json({ message: 'Internal server error.' });
   }
